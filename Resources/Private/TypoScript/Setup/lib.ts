303f878422d590a316553a23a3633c2639e21e3b
Philipp Dieter <philipp.dieter@attic-media.de>
2019-10-17 13:15:59 +0200

    [TASK] Update naming, keep compatibility

--- Resources/Private/TypoScript/Setup/lib.ts
+++ Resources/Private/TypoScript/Setup/lib.ts
@@ -1,8 +1,14 @@
 lib.rootpid = TEXT
 lib.rootpid.data = leveluid : 0
 
+lib.pidRoot = TEXT
+lib.pidRoot.data = leveluid : 0
+
 lib.currentLevel = TEXT
 lib.currentLevel.data = level:1
 
 lib.currentPid = TEXT
 lib.currentPid.data = TSFE:id
+
+lib.pidCurrent = TEXT
+lib.pidCurrent.data = TSFE:id
