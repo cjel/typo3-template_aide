95da3b7e589b87b91ddc2ffc6fdef3c731910c5d
Philipp Dieter <philipp.dieter@attic-media.de>
2020-04-16 23:40:02 +0200

    [TASK] Add getClassViewHelper

---
+++ Classes/ViewHelpers/GetClassViewHelper.php
@@ -0,0 +1,17 @@
+<?php
+namespace Cjel\TemplatesAide\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class GetClassViewHelper extends AbstractViewHelper
+{
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        return (new \ReflectionClass($renderChildrenClosure()))->getShortName();
+    }
+}
