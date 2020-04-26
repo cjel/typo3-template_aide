d99c831bbb81604945c3a608cc9c260e5124d55b
Philipp Dieter <philipp.dieter@attic-media.de>
2020-04-27 01:01:55 +0200

    [BUGFIX] Remove empty table specification

--- ext_tables.sql
+++
@@ -1,8 +0,0 @@
-#
-# Table structure for table 'tx_templatesaide_domain_model_dummy'
-#
-CREATE TABLE tx_templatesaide_domain_model_dummy (
-
-);
-
-## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
