cf3c2a08e83b3714da7d3de31ace303b5ff06396
Philipp Dieter <philipp.dieter@attic-media.de>
2020-07-02 11:59:02 +0200

    [TASK] Add config for folder tree

--- Resources/Private/UserTSConfig/default.ts
+++ Resources/Private/UserTSConfig/default.ts
@@ -1,2 +1,9 @@
 #options.pageTree.showPageIdWithTitle = 1
 options.pageTree.showNavTitle = 1
+
+options {
+    folderTree {
+        uploadFieldsInLinkBrowser = 0
+        hideCreateFolder = 1
+    }
+}
