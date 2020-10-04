8f24164f031c6a80b16cad64f30fdaada205a7d7
Philipp Dieter <philipp.dieter@attic-media.de>
2020-10-05 00:56:52 +0200

    [TASK] add RandomStringUtility

---
+++ Classes/Utility/RandomStringUtility.php
@@ -0,0 +1,32 @@
+<?php
+namespace Cjel\TemplatesAide\Utility;
+
+/***
+ *
+ * This file is part of the "" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2019 Philipp Dieter <philipp@glanzstueck.agency>, Glanzstück GmbH
+ *
+ ***/
+
+class RandomStringUtility
+{
+    public static function getToken(
+        int $length = 64,
+        string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ): string {
+        if ($length < 1) {
+            throw new \RangeException("Length must be a positive integer");
+        }
+        $pieces = [];
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $pieces []= $keyspace[random_int(0, $max)];
+        }
+        return implode('', $pieces);
+    }
+
+}
