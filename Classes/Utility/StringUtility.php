a3cb4325bf06bed99e8df944aea008e3ea2df8a1
Philipp Dieter <philipp.dieter@attic-media.de>
2021-03-04 10:56:21 +0100

    [TASK] Add random string method

--- Classes/Utility/StringUtility.php
+++ Classes/Utility/StringUtility.php
@@ -45,4 +45,23 @@ class StringUtility
         return $string;
     }
 
+    function getRandomString(
+        int $length = 64,
+        string $keyspace = null
+    ): string {
+        if (!$keyspace) {
+            $keyspace = '0123456789'
+                . 'abcdefghijklmnopqrstuvwxyz'
+                . 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+        if ($length < 1) {
+            throw new \RangeException("Length must be a positive integer");
+        }
+        $pieces = [];
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $pieces []= $keyspace[random_int(0, $max)];
+        }
+        return implode('', $pieces);
+    }
 }
